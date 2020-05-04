14ad698844985a506e10498d9805c3ba36367525
Marcelo do Rosario Silva <marcelo.rosario.silva@gmailcom>
2020-05-04 19:38:30 -0300

    commit api.js frontend

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ console.log(process.env.PORT);
 console.log(process.env.BASE_URL);
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://2b2fabbb.ngrok.io',
   headers:{
     'Content-Type': 'application/json'
   }
